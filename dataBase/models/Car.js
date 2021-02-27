1fda47463ddc12862611b0271c86ce75e5aecde0
Allex372 <62441924+Allex372@users.noreply.github.com>
2021-02-28 00:36:54 +0200

    Initial commit

---
+++ dataBase/models/Car.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const ownerSubScheme = {
+    number: { type: Number, required: true }
+};
+
+const carScheme = new Schema({
+    model: { type: String, required: true },
+    year: { type: Number, default: 1999 },
+    _owner: [{ type: Schema.Types.ObjectId }]
+}, { toObject: { virtuals: true }, toJSON: { virtuals: true } });
+
+carScheme.virtual('full').get(function() {
+    return `${this.model} ${this.year}`;
+});
+
+carScheme.virtual('carOwners', {
+    ref: 'Owner',
+    localField: 'owners',
+    foreignField: '_id',
+    justOne: true
+});
+
+carScheme.pre('find', function() {
+    this.populate('carOwners');
+});
+
+module.exports = model('Cars', carScheme);
---
+++ dataBase/models/User.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const carSubScheme = {
+    model: { type: String },
+    price: { type: Number }
+};
+
+const userScheme = new Schema({
+    name: { type: String, required: true },
+    age: { type: Number, default: 18 },
+    _cars: [{ type: Schema.Types.ObjectId }]
+}, { toObject: { virtuals: true }, toJSON: { virtuals: true } });
+
+userScheme.virtual('full_name').get(function() {
+    return `${this.name} ${this.password}`;
+});
+
+userScheme.virtual('userCars', {
+    ref: 'Car',
+    localField: 'cars',
+    foreignField: '_id'
+});
+
+userScheme.pre('find', function() {
+    this.populate('userCars');
+});
+
+module.exports = model('User', userScheme);
